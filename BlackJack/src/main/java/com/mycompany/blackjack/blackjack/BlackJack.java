465adf8af7476ca5e2ed77394d6aee191fc041a5
Summer Projects <khadkaroshan441@gmail.com>
2024-06-09 15:07:34 -0400

    Update BlackJack.java

--- BlackJack/src/main/java/com/mycompany/blackjack/blackjack/BlackJack.java
+++ BlackJack/src/main/java/com/mycompany/blackjack/blackjack/BlackJack.java
@@ -9,9 +9,20 @@ package com.mycompany.blackjack.blackjack;
  *
  * @author 97798
  */
+/**
+ * The BlackJack class contains the main method to start the Blackjack game.
+ * This is a simple placeholder class that currently prints "Hello World!" to the console.
+ */
 public class BlackJack {
 
+    /**
+     * The main method is the entry point of the program.
+     * It prints "Hello World!" to the console.
+     * 
+     * @param args Command line arguments (not used in this program)
+     */
     public static void main(String[] args) {
+        // Print a greeting message to the console
         System.out.println("Hello World!");
     }
 }
